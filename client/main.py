fea543e23d41ae521e4e00c4515047775db42e4a
nekresh <nekresh@gmail.com>
2014-01-11 19:45:29 +0100

    Start using APScheduler

--- client/main.py
+++ client/main.py
@@ -1,6 +1,13 @@
-import sensors
+from apscheduler.scheduler import Scheduler
 
-d = dict()
-d["address"] = "http://python.org"
-sensor = sensors.SpeedSensor(**d)
-print(sensor.sense())
+sched = Scheduler(standalone=True)
+
+@sched.interval_schedule(minutes=1)
+def execute_sensors():
+	print("Kikoo")
+
+try:
+	sched.start()
+except KeyboardInterrupt:
+	print "Exitting !"
+	sched.shutdown(wait=True)
